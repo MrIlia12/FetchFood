b77445b83b1489efdb4a76110489b788bfb48644
lil-drow <lil_drow@outlook.com>
2025-08-26 01:55:03 +0500

    Описана точка входа для работы бота: Добавила папки Models и Services в структуру проекта. Добавила файл StaticNames для хранения и переиспользования часто встречающихся значений. Описала работу тг бота с базовыми командами: старт приёма сообщений, остановка приёма, обработка команд /start и /help. Токен бота вынесла в json.

---
+++ FetchFood/Models/SettingsModel.cs
@@ -0,0 +1,11 @@
+﻿namespace FetchFood.Models
+{
+    public class Settings
+    {
+        public TelegramSettings Telegram { get; set; } = new();
+    }
+    public class TelegramSettings
+    {
+        public string BotToken { get; set; } = String.Empty;
+    }
+}
