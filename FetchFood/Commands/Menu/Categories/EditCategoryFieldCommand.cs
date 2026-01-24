5b349e4153506cbae8c052c20100766b7ed78db0
lil-drow <lil_drow@outlook.com>
2026-01-25 01:11:51 +0500

    Команды редактирования категорий объединены в один файл
    Лишние файлы удалены

--- FetchFood/Commands/Menu/Categories/EditCategoryFieldCommand.cs
+++ FetchFood/Commands/Menu/Categories/EditCategoryFieldCommand.cs
@@ -63,4 +63,39 @@ namespace FetchFood.Commands.Menu.Categories
         /// </summary>
         protected abstract Task<bool> ApplyValueAsync(MenuCommandContext ctx, DataAccess.Entities.PositionCategory category, string value);
     }
+
+    /// <summary>
+    /// Команда изменения названия категории.
+    /// </summary>
+    public class EditCategoryNameCommand : EditCategoryFieldCommand
+    {
+        public override string CommandKey => BotCommands.EDIT_CATEGORY_NAME;
+        protected override string FieldNameRu => "название";
+
+        protected override Task<bool> ApplyValueAsync(MenuCommandContext ctx, DataAccess.Entities.PositionCategory category, string value)
+        {
+            if (value.Length > 100)
+            {
+                SendMessageAsync(ctx, "Название должно быть <= 100 символов.");
+                return Task.FromResult(false);
+            }
+            category.Name = value;
+            return Task.FromResult(true);
+        }
+    }
+
+    /// <summary>
+    /// Команда изменения описания категории.
+    /// </summary>
+    public class EditCategoryDescCommand : EditCategoryFieldCommand
+    {
+        public override string CommandKey => BotCommands.EDIT_CATEGORY_DESC;
+        protected override string FieldNameRu => "описание";
+
+        protected override Task<bool> ApplyValueAsync(MenuCommandContext ctx, DataAccess.Entities.PositionCategory category, string value)
+        {
+            category.Description = value;
+            return Task.FromResult(true);
+        }
+    }
 }
